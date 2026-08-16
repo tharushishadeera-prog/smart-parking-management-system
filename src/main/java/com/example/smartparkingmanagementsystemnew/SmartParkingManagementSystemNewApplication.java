f98bbb8bee5cea7fac82c2050d87983f0068fd17
tharushishadeera-prog <tharushishadeera@gmail.com>
2026-08-16 08:58:53 +0530

    Initial microservices setup

---
+++ src/main/java/com/example/smartparkingmanagementsystemnew/SmartParkingManagementSystemNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartparkingmanagementsystemnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingManagementSystemNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkingManagementSystemNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartparkingmanagementsystemnew/SmartParkingManagementSystemNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartparkingmanagementsystemnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingManagementSystemNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
